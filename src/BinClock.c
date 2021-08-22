28674f29d4b3933aa8bd73c4a1856eb45b59b241
Samoei <samoeiayuka@gmail.com>
2021-08-22 22:59:46 +0100

    Removed default time

--- src/BinClock.c
+++ src/BinClock.c
@@ -88,9 +88,9 @@ int main(void){
 
 	//Set random time (3:04PM)
 	//You can comment this file out later
-	wiringPiI2CWriteReg8(RTC, HOUR_REGISTER, 0x13+TIMEZONE); // write hour
-	wiringPiI2CWriteReg8(RTC, MIN_REGISTER, 0x4);
-	wiringPiI2CWriteReg8(RTC, SEC_REGISTER, 0x00);
+	//wiringPiI2CWriteReg8(RTC, HOUR_REGISTER, 0x13+TIMEZONE); // write hour
+	//wiringPiI2CWriteReg8(RTC, MIN_REGISTER, 0x4);
+	//wiringPiI2CWriteReg8(RTC, SEC_REGISTER, 0x00);
 
 	//int led_status = 0; // off initially
 	toggleTime();
